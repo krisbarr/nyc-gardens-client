ce81b82b4c29ecaa884a8f2e1f9d7149f15dbff9
kris.leanne.barrett@gmail.com <scripts/git.sh>
2021-06-07 15:53:08 -0400

    Fix api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://vast-reef-96032.herokuapp.com/',
+  production: 'https://vast-reef-96032.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
